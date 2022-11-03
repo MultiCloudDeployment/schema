8f268d88408165a952e0de6491c17a351abf9b53
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-03 14:30:26 -0500

    Added Arm and Bicep templates, as well as some loose documentation

--- Azure/Resources/AutomationAccount.cs
+++ Azure/Resources/AutomationAccount.cs
@@ -10,6 +10,17 @@ using System.Text.Json;
 
 namespace MCD.Azure.Resources
 {
+    /// <summary>
+    /// AutomationAccount Class
+    /// 
+    /// This contains all the properties and methods in order to
+    /// create an AutomationAccount using any one of the supported
+    /// deployment languages.
+    /// 
+    /// Arm
+    /// Bicep
+    /// Terraform
+    /// </summary>
     public class AutomationAccount
     {
         const bool DefaultDisableLocalAuth = false;
@@ -21,6 +32,10 @@ namespace MCD.Azure.Resources
             PublicNetworkAccess = DefaultPublicNetworkAccess;
             Tags = new List<Tag>();
         }
+        /// <summary>
+        /// Create an AutomationAccount object from JSON
+        /// </summary>
+        /// <param name="json"></param>
         public AutomationAccount(string json)
         {
             AutomationAccount resource = JsonSerializer.Deserialize<AutomationAccount>(json);
@@ -87,6 +102,12 @@ namespace MCD.Azure.Resources
         }
         [Required]
         public string ResourceGroup { get; set; }
+        /// <summary>
+        /// ToString() Override
+        /// </summary>
+        /// <param name="deploymentType"></param>
+        /// <param name="deploymentId"></param>
+        /// <returns>Returns either an ARM parameterfile, Bicep file, or Terraform module</returns>
         public string ToString(Azure.DeploymentType deploymentType, string deploymentId)
         {
             Azure.DeploymentType TemplateLanguage = deploymentType;
@@ -97,6 +118,58 @@ namespace MCD.Azure.Resources
 
             switch (TemplateLanguage.Name.ToLower())
             {
+                case "arm":
+                    buffer.AppendLine("{");
+                    buffer.AppendLine(" \"$schema\": \"http://schema.management.azure.com/schemas/2019-04-01/deploymentParameters.json#\",");
+                    buffer.AppendLine(" \"contentVersion\": \"1.0.0.4\",");
+                    buffer.AppendLine(" \"parameters\": {");
+                    buffer.AppendLine("  \"name\": {");
+                    buffer.AppendLine($"   \"value\": \"{this.Name}\"");
+                    buffer.AppendLine("  },");
+                    buffer.AppendLine("  \"location\": {");
+                    buffer.AppendLine($"   \"value\": \"{this.Location}\"");
+                    buffer.AppendLine("  },");
+                    buffer.AppendLine("  \"tags\": {");
+                    if (this.Tags.Count > 0)
+                    {
+                        buffer.AppendLine($"   \"value\": {{{this.Tags}}}");
+                    }
+                    else
+                    {
+                        buffer.AppendLine("   \"value\": {}");
+                    }
+                    buffer.AppendLine("  },");
+                    buffer.AppendLine("  \"SkuName\": {");
+                    buffer.AppendLine($"   \"value\": \"{this.SkuName.Name}\"");
+                    buffer.AppendLine("  \"DisableLocalAuth\": {");
+                    buffer.AppendLine($"   \"value\": {this.DisableLocalAuth}");
+                    buffer.AppendLine("  },");
+                    buffer.AppendLine("  \"PublicNetworkAccess\": {");
+                    buffer.AppendLine($"   \"value\": {this.PublicNetworkAccess}");
+                    buffer.AppendLine("  }");
+                    buffer.AppendLine("  \"ResourceGroupName\": {");
+                    buffer.AppendLine($"   \"value\": {this.ResourceGroup}");
+                    buffer.AppendLine("  }");
+                    buffer.AppendLine(" }");
+                    buffer.AppendLine("}");
+                    break;
+                case "bicep":
+                    buffer.AppendLine("module AutomationAccount 'automationaccount.bicep' = {");
+                    buffer.AppendLine($"  name: \"{this.Name}-deployment\"");
+                    buffer.AppendLine($"  scope: resourceGroup({this.ResourceGroup})");
+                    buffer.AppendLine("  params: {");
+                    buffer.AppendLine($"    name: \"{this.Name}\"");
+                    buffer.AppendLine($"    location: \"{this.Location}\"");
+                    if (this.Tags.Count > 0)
+                    {
+                        buffer.AppendLine($"    tags: {this.Tags}");
+                    }
+                    buffer.AppendLine($"    DisableLocalAuth: {this.DisableLocalAuth}");
+                    buffer.AppendLine($"    PublicNetworkAccess: {this.PublicNetworkAccess}");
+                    buffer.AppendLine($"    SkuName: \"{this.SkuName.Name}\"");
+                    buffer.AppendLine("  }");
+                    buffer.AppendLine("}");
+                    break;
                 case "terraform":
                     string ModuleName = "aa-" + this.Name + "-" + DeploymentId;
 
@@ -119,12 +192,14 @@ namespace MCD.Azure.Resources
                     }
                     buffer.AppendLine("}");
                     break;
-                case "bicep":
-                    break;
             }
 
             return buffer.ToString();
         }
+        /// <summary>
+        /// Cast a JSON object to an AutomationAccount
+        /// </summary>
+        /// <param name="json"></param>
         public static explicit operator AutomationAccount(string json)
         {
             return new AutomationAccount(json);
