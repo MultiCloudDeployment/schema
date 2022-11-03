cc9502b3467a26f5e73d065d8af3117187717a1b
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-03 13:40:16 -0500

    Added the class enum for DeploymentType

--- Azure/Azure.cs
+++ Azure/Azure.cs
@@ -7,9 +7,18 @@ namespace MCD.Azure
     {
         public Azure()
         {
+            DeploymentLanguage = DeploymentType.Terraform;
             Tenant = new();
         }
-        public string DeploymentLanguage { get; set; }
+        public DeploymentType DeploymentLanguage { get; set; }
         public Tenant Tenant { get; set; }
+        public class DeploymentType
+        {
+            private DeploymentType(string value) { Name = value; }
+            public string Name { get; private set; }
+            public static DeploymentType Arm { get { return new DeploymentType("Arm"); } }
+            public static DeploymentType Bicep { get { return new DeploymentType("Bicep"); } }
+            public static DeploymentType Terraform { get { return new DeploymentType("Terraform"); } }
+        }
     }
 }
